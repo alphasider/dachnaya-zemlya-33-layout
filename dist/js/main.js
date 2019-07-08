c1c2982a4e850c96217f4023ffd5606eb917a2cb
Rustam <rustamergashev.sp@gmail.com>
2019-07-08 10:08:39 +0500

    Добавлены табы (вкладки) на стр. новостей | Начаты работы над слайдером на стр. участок

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,4 @@
+// Меню бургер
 const menuBtn = document.querySelector('.menu-btn');
 const headerNav = document.querySelector('.header-nav');
 const headerMenu = document.querySelector('.header-menu');
@@ -25,9 +26,60 @@ function toggleMenu() {
   }
 }
 
+
+const tablinks = document.querySelectorAll('.articles-tabs__item'); // Вкладки
+const tabcontent = document.querySelectorAll('.articles-content__item'); // Содержимое
+
+// Табы на странице новости
+function switchTab(e, id) {
+
+  // let evTar = e.target;
+  // let curTar = e.currentTarget;
+  let clickedTab = e.target.attributes['id'].value;
+  let activeTabContent = document.querySelector('.articles-content__item.' + clickedTab);
+
+  // Скрытие всех данных
+  for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Удаление активных классов
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" articles-tabs__item_active", "");
+  }
+
+  // console.log(evTar, curTar);
+
+  activeTabContent.style.display = "block";
+  e.currentTarget.className += " articles-tabs__item_active";
+}
+
+// const tablinks = document.querySelector('.articles-tabs');
+for (let i = 0; i < tablinks.length; i++) {
+  tablinks[i].addEventListener('click', switchTab);
+}
+
+
+
 // Слайдер: Slick.js
 $(document).ready(function () {
+
+  // Слайдер на главной странице
   $('.home-slider').slick({
+    infinite: false,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [{
+      breakpoint: 1199,
+      settings: {
+        arrows: false
+      }
+    }]
+  });
+
+  // Слайдер на странице участка
+  $('.product-slider').slick({
     infinite: false,
     dots: true,
     slidesToShow: 1,
